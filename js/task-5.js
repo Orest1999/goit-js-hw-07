d5936dba3e4e0346a397555562dcc40007e66c7f
orestpol <orest.polagnyuk@gmail.com>
2025-09-01 08:47:03 +0300

    fwfwfwotototot

--- js/task-5.js
+++ js/task-5.js
@@ -4,6 +4,7 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+
 const changeColorBtn = document.querySelector(".change-color");
 const colorSpan = document.querySelector(".color");
 const body = document.body;
